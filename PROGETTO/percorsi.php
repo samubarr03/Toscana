284a6b03bbeea143e22ff42482a4c12db64f8c7f
samubarr03 <98096249+samubarr03@users.noreply.github.com>
2022-05-05 14:04:35 +0200

    Co-authored-by: Notepadmerda <Notepadmerda@users.noreply.github.com> Co-authored-by: danielesacco <danielesacco@users.noreply.github.com>

---
+++ PROGETTO/percorsi.php
@@ -0,0 +1,123 @@
+<!DOCTYPE html>
+
+<head>
+  <br><br><br>
+<?php include "includes/header.php";?>
+
+  <meta charset="utf-8">
+  <meta content="width=device-width, initial-scale=1.0" name="viewport">
+
+  <title>Il nostro TEAM</title>
+
+  <!-- Importa CSS -->
+  <link href="css/aos.css" rel="stylesheet">
+  <link href="css/bootstrap.min.css" rel="stylesheet">
+  <link href="css/bootstrap-icons.css" rel="stylesheet">
+  <link href="css/boxicons.min.css" rel="stylesheet">
+  <link href="css/glightbox.min.css" rel="stylesheet">
+  <link href="css/remixicon.css" rel="stylesheet">
+  <link href="css/swiper-bundle.min.css" rel="stylesheet">
+  <link href="css/style.css" rel="stylesheet">
+
+  <link href="img/toscana.png" rel="icon">
+
+</head>
+<style>
+    .card {
+  box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2);
+  max-width: 400px;
+  margin: auto;
+  text-align: center;
+  font-family: arial;
+  background-color: #dfffde;
+}
+
+.price {
+  color: #009970;
+  font-size: 200%;
+}
+
+.card button {
+  border: none;
+  outline: 0;
+  padding: 12px;
+  color: white;
+  background-color: #009970;
+  text-align: center;
+  cursor: pointer;
+  width: 60%;
+  font-size: 18px;
+}
+
+.card button:hover {
+  opacity: 0.7;
+}
+#hero h1 {
+  margin: 0 0 10px 0;
+  font-size: 48px;
+  font-weight: 700;
+  line-height: 56px;
+  color: #011a00;
+}
+#hero p{
+    margin: 0 0 3px 0;
+    font-size: 20px;
+    font-weight: 700;
+    color: #000; 
+}
+#hero .price {
+  color: #009970;
+  font-size: 200%;
+}
+</style>
+<body>
+        
+<!-- ======= Hero Section ======= -->
+<section id="hero" class="d-flex align-items-center" style="background:url(img/hero_toscana.jpg)">
+    <div class="container text-center position-relative" data-aos="fade-in" data-aos-delay="200">
+    <div class="Percorsi" >
+            <div class="card" style="float: left; position:relative ">
+            <img src="img/pisatower.jpg" alt="Denim Jeans" style="width:100%">
+            <h1>Tour base</h1>
+            <p class="price">GRATIS</p>
+            <p>Questo percorso include tutte le attrazioni della Toscana, andando a toccare i cardini della Toscana in generale.</p>
+            <p><button>Vai subito</button></p>
+            </div>
+
+            <div class="card" style="float: left; margin-left:3%;">
+            <img src="img/firenzegiotto.webp" alt="Denim Jeans" style="width:100%">
+            <h1>Tour master</h1>
+            <p class="price">$9.99</p>
+            <p>Include la maggior parte delle attrazioni Toscane e vi offrirà un'immersione in maniera approfondita.</p>
+            <p><button>Compra</button></p>
+            </div>
+
+            <div class="card" style="float: left; margin-left:3%;">
+            <img src="img/collagetoscana.jpg" alt="Denim Jeans" style="float-left:5%; width:90%; float-left:5%;">
+            <h1>Advancend tour</h1>
+            <p class="price"><font color="red"><DEL>$19.99</del></font> $14.99</p>
+            <p>Questo tour comprende TUTTI i punti di interesse della nostra amata Toscana e vi farà conoscere tuttI i segreti della regione.</p>
+            <p><button>Compra</button></p>
+            </div>
+            <div style="clear: both">
+    </div>
+  </section><!-- End Hero -->
+  <!--  <img src="img/hero_toscana.jpg"> -->
+        
+      <?php include "includes/footer.html";?>
+
+      <a href="#" class="back-to-top d-flex align-items-center justify-content-center"><i class="bi bi-arrow-up-short"></i></a>
+
+
+<!-- Importa JS-->
+<script src="js/purecounter.js"></script>
+<script src="js/aos.js"></script>
+<script src="js/bootstrap.bundle.min.js"></script>
+<script src="js/glightbox.min.js"></script>
+<script src="js/isotope.pkgd.min.js"></script>
+<script src="js/swiper-bundle.min.js"></script>
+<script src="js/validate.js"></script>
+<script src="js/main.js"></script>
+
+</body>
+</html>
--- PROGETTO/team.php
+++ PROGETTO/team.php
@@ -1,33 +1,39 @@
 <!DOCTYPE html>
 
 <head>
-<?php include "includes/header.php";?>
-
   <meta charset="utf-8">
   <meta content="width=device-width, initial-scale=1.0" name="viewport">
 
-  <title>Il nostro TEAM</title>
+  <title>Toscana Tour</title>
 
-  <!-- Importa CSS -->
-  <link href="css/aos.css" rel="stylesheet">
-  <link href="css/bootstrap.min.css" rel="stylesheet">
-  <link href="css/bootstrap-icons.css" rel="stylesheet">
-  <link href="css/boxicons.min.css" rel="stylesheet">
-  <link href="css/glightbox.min.css" rel="stylesheet">
-  <link href="css/remixicon.css" rel="stylesheet">
-  <link href="css/swiper-bundle.min.css" rel="stylesheet">
-  <link href="css/style.css" rel="stylesheet">
-
-  <link href="img/toscana.png" rel="icon">
+  <?php include "includes/css.html"; ?>
 
 </head>
 
 <body>
+
+  <?php include "includes/header.php";?>
+
+  <!-- ======= Hero Section ======= -->
+  <section id="hero" class="d-flex align-items-center" style="background:url(img/hero_toscana.jpg)">
+    <div class="container text-center position-relative" data-aos="fade-in" data-aos-delay="200">
+      <h1>IL NOSTRO TEAM</h1>
+      <h2>I migliori percorsi e luoghi da visitare della Toscana</h2>
+      <a href="tour.php" class="btn-get-started scrollto">VAI AI TOUR</a>
+    </div>
+  </section><!-- End Hero -->
+
+  <main id="main">
+    
+  <?php include "includes/clienti.html";?>
+
+
+
     <section id="team" class="team">
       <div class="container">
 
         <div class="row">
-          <div class="col-lg-4">
+          <div class="col-lg-8">
             <div class="section-title" data-aos="fade-right">
               <h2>Team</h2>
               <p>Le persone che hanno lavorato a questo progetto.</p>
@@ -36,42 +42,44 @@
         
           <div class="col-lg-8">
             <div class="row">
-
+              <!--  SACCO -->
               <div class="col-lg-6">
                 <div class="member" data-aos="zoom-in" data-aos-delay="100">
-                  <div class="pic"><img src="assets/img/team/team-1.jpg" class="img-fluid" alt=""></div>
+                  <div class="pic"><img src="img/.jpg" class="img-fluid" alt=""></div>
                   <div class="member-info">
                     <h4>Daniele Sacco</h4>
                     <span>Chief Executive Officer</span>
                     <p>Explicabo voluptatem mollitia et repellat qui dolorum quasi</p>
                     <div class="social">
-                      <a href=""><i class="ri-twitter-fill"></i></a>
-                      <a href=""><i class="ri-facebook-fill"></i></a>
-                      <a href=""><i class="ri-instagram-fill"></i></a>
-                      <a href=""> <i class="ri-linkedin-box-fill"></i> </a>
+                      <a href=""><i class="bi bi-instagram"></i></a>
+                      <a href=""><i class="bi bi-facebook"></i></a>
+                      <a href=""><i class="bi bi-snapchat"></i></a>
+                      <a href=""><i class="bi bi-currency-euro"></i> </a>
                     </div>
                   </div>
                 </div>
-              </div>
+              </div><!--  fine SACCO -->
 
+              <!--  CUSA -->
               <div class="col-lg-6 mt-4 mt-lg-0">
                 <div class="member" data-aos="zoom-in" data-aos-delay="200">
-                  <div class="pic"><img src="assets/img/team/team-2.jpg" class="img-fluid" alt=""></div>
+                  <div class="pic"><img src="img/.jpg" class="img-fluid" alt=""></div>
                   <div class="member-info">
                     <h4>Leonardo Cusa</h4>
                     <span>Product Manager</span>
                     <p>Aut maiores voluptates amet et quis praesentium qui senda para</p>
                     <div class="social">
-                      <a href=""><i class="ri-twitter-fill"></i></a>
-                      <a href=""><i class="ri-facebook-fill"></i></a>
-                      <a href=""><i class="ri-instagram-fill"></i></a>
-                      <a href=""> <i class="ri-linkedin-box-fill"></i> </a>
+                      <a href=""><i class="bi bi-instagram"></i></a>
+                      <a href=""><i class="bi bi-facebook"></i></a>
+                      <a href=""><i class="bi bi-snapchat"></i></a>
+                      <a href=""><i class="bi bi-currency-euro"></i> </a>
                     </div>
                   </div>
                 </div>
-              </div>
+              </div><!--  fine CUSA -->
 
-              <div class="col-lg-6 mt-4">
+              <!--  BARRAGO -->
+              <div class="col-lg-6 mt-4 mt-lg-0">
                 <div class="member" data-aos="zoom-in" data-aos-delay="300">
                   <div class="pic"><img src="assets/img/team/team-3.jpg" class="img-fluid" alt=""></div>
                   <div class="member-info">
@@ -79,14 +87,14 @@
                     <span>CTO</span>
                     <p>Quisquam facilis cum velit laborum corrupti fuga rerum quia</p>
                     <div class="social">
-                      <a href=""><i class="ri-twitter-fill"></i></a>
-                      <a href=""><i class="ri-facebook-fill"></i></a>
-                      <a href=""><i class="ri-instagram-fill"></i></a>
-                      <a href=""> <i class="ri-linkedin-box-fill"></i> </a>
+                      <a href=""><i class="bi bi-instagram"></i></a>
+                      <a href=""><i class="bi bi-facebook"></i></a>
+                      <a href=""><i class="bi bi-snapchat"></i></a>
+                      <a href=""><i class="bi bi-currency-euro"></i> </a>
                     </div>
                   </div>
                 </div>
-              </div>
+              </div><!--  fine BARRAGO -->
 
             </div>
 
@@ -94,20 +102,24 @@
         </div>
 
       </div>
-      <?php include "includes/footer.html";?>
+    </section>
 
-<a href="#" class="back-to-top d-flex align-items-center justify-content-center"><i class="bi bi-arrow-up-short"></i></a>
+  </main>
 
+  <?php include "includes/footer.html";?>
 
-<!-- Importa JS-->
-<script src="js/purecounter.js"></script>
-<script src="js/aos.js"></script>
-<script src="js/bootstrap.bundle.min.js"></script>
-<script src="js/glightbox.min.js"></script>
-<script src="js/isotope.pkgd.min.js"></script>
-<script src="js/swiper-bundle.min.js"></script>
-<script src="js/validate.js"></script>
-<script src="js/main.js"></script>
+  <a href="#" class="back-to-top d-flex align-items-center justify-content-center"><i class="bi bi-arrow-up-short"></i></a>
+
+  
+  <!-- Importa JS-->
+  <script src="js/purecounter.js"></script>
+  <script src="js/aos.js"></script>
+  <script src="js/bootstrap.bundle.min.js"></script>
+  <script src="js/glightbox.min.js"></script>
+  <script src="js/isotope.pkgd.min.js"></script>
+  <script src="js/swiper-bundle.min.js"></script>
+  <script src="js/validate.js"></script>
+  <script src="js/main.js"></script>
 
 </body>
 </html>
