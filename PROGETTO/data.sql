b43b95987e3d1806de82bc5a05edb02ecfe21a5d
samubarr03 <98096249+samubarr03@users.noreply.github.com>
2022-05-06 11:30:27 +0200

    main

--- PROGETTO/data.sql
+++ PROGETTO/data.sql
@@ -2,7 +2,8 @@
 CREATE TABLE Citta(
 id int AUTO_INCREMENT PRIMARY KEY,
 nome varchar(255) NOT NULL,
-descrizione varchar(255) NOT NULL
+descrizione varchar(255) NOT NULL,
+img varchar(255)
 );
 
 
@@ -15,6 +16,7 @@ CREATE TABLE Attrazione(
 id int AUTO_INCREMENT PRIMARY KEY,
 descrizione varchar(255) NOT NULL,
 immagine varchar(255) NOT NULL,
+img varchar(255),
 qr_code varchar(255) UNIQUE,
 nome_tipologia varchar(255) REFERENCES Tipologia(nome),
 idCitta int REFERENCES Citta(id)
@@ -74,4 +76,15 @@ idAttrazione int REFERENCES Attrazione(id),
 PRIMARY KEY(idPercorso, idAttrazione)
 );
  
-INSERT INTO Citta VALUES (id, 'Firenze', 'bella città' );
+INSERT INTO Citta VALUES (id, 'Firenze', 'La città di Firenze, fondata sulle sponde del fiume Arno, è il capoluogo della Toscana ed è il centro dell'area metropolitana Firenze-Prato-Pistoia, nonché l'ottavo comune in Italia per popolazione ( circa 380 mila abitanti ).', '' );
+INSERT INTO Citta VALUES (id, 'Pisa', 'bella città', '' );
+INSERT INTO Citta VALUES (id, 'Siena', 'bella città', '' );
+INSERT INTO Citta VALUES (id, 'Volterra', 'bella città', '' );
+INSERT INTO Citta VALUES (id, 'Livorno', 'bella città', '' );
+INSERT INTO Citta VALUES (id, 'San Gimignano', 'bella città', '' );
+INSERT INTO Citta VALUES (id, 'Grosseto', 'bella città', '' );
+INSERT INTO Citta VALUES (id, 'Prato', 'bella città', '' );
+INSERT INTO Citta VALUES (id, 'Massa', 'bella città', '' );
+INSERT INTO Citta VALUES (id, 'Carrara', 'bella città', '' );
+INSERT INTO Citta VALUES (id, 'Lucca', 'bella città', '' );
+INSERT INTO Citta VALUES (id, 'Arezzo', 'bella città', '' );
