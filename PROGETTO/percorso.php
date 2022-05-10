49e667a518ae976be0f19033747c444a17cb8556
samubarr03 <98096249+samubarr03@users.noreply.github.com>
2022-05-10 11:31:01 +0200

    main
    Co-authored-by: Notepadmerda <Notepadmerda@users.noreply.github.com>
    Co-authored-by: danielesacco <danielesacco@users.noreply.github.com>

---
+++ PROGETTO/percorso.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+
+<head>
+  <meta charset="utf-8">
+  <meta content="width=device-width, initial-scale=1.0" name="viewport">
+
+  <title>[NOME PERCORSO]</title>
+
+  <?php include "includes/css.html"; ?>
+
+</head>
+<body>
+    <!--<?php include "includes/header.php";?>-->
+    <div><h1>[Nome percorso]</h1></div> 
+    <div class="GoogleMaps">
+    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d46146.880638254755!2d10.360358803677867!3d43.7068074009471!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x12d5919af0f6598f%3A0xaab80fb5a78478c8!2sPisa%20PI!5e0!3m2!1sit!2sit!4v1652174831666!5m2!1sit!2sit" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+    </div>
+    <div class="PartePHP">
+        INSERISCI QUI
+    </div>
+    <div class="InfoEQR">
+
+    </div>
+    <div class="BarraDiCompletamento">
+
+    </div>
