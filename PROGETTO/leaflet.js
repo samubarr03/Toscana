553af7cb0680ff2c8c8e9782e86495be5875e649
samubarr03 <98096249+samubarr03@users.noreply.github.com>
2022-05-17 11:31:24 +0200

    Co-authored-by: danielesacco <danielesacco@users.noreply.github.com> Co-authored-by: Notepadmerda <Notepadmerda@users.noreply.github.com>

--- PROGETTO/leaflet.js
+++ PROGETTO/leaflet.js
@@ -8,31 +8,29 @@ var map = L.map('map').setView([43.65, 11.13], 8); // latitudine = 43.4586541 ; 
 
 
    // STYLE NON ANCORA MESSO-->    mapbox://styles/notepadmerda/cl342x7zn005415p215vvr20e
-   var LeafIcon = L.Icon.extend({
-        options: {
-            shadowUrl: 'img/Tracker/Ombra.png',
-            iconAnchor:   [0, 0],
-            shadowAnchor: [0, 0],
-            popupAnchor:  [30, 0],
-            iconSize:     [60, 60],
-            shadowSize:   [60, 60]
-        }
+   
+    var Bianco = L.icon({
+        iconUrl: 'img/Tracker/Bianco.png',
+        shadowUrl:'img/Tracker/Ombra.png',
+        iconSize:     [60, 60], // size of the icon
+        shadowSize:   [60, 60], // size of the shadow
+        iconAnchor:   [1, 1], // point of the icon which will correspond to marker's location
+        shadowAnchor: [1, 1],  // the same for the shadow
+        popupAnchor:  [0, 0] // point from which the popup should open relative to the iconAncho
     });
-    var Bianco = new LeafIcon({iconUrl: 'img/Tracker/Bianco.png'}),
-    Blu = new LeafIcon({iconUrl: 'img/Tracker/Blu.png'}),
-    Ciano = new LeafIcon({iconUrl: 'img/Tracker/Ciano.png'}),
-    Rosso = new LeafIcon({iconUrl: 'img/Tracker/Rosso.png'}),
-    Verde = new LeafIcon({iconUrl: 'img/Tracker/Verde.png'}),
-    Viola = new LeafIcon({iconUrl: 'img/Tracker/Viola.png', iconAnchor: [0, 0]});
-
+    var Blu = L.icon({
+        iconUrl: 'img/Tracker/Blu.png',
+        shadowUrl:'img/Tracker/Ombra.png',
+        iconSize:     [60, 60], // size of the icon
+        shadowSize:   [60, 60],
+        iconAnchor:   [30, 30], // point of the icon which will correspond to marker's location
+        shadowAnchor: [30, 30],  // the same for the shadow
+        popupAnchor:  [0, 0] // size of the shadow
+        });
     //var cities = L.layerGroup([torre_di_pisa, piazza_del_duomo, ponte_vecchio, campanile_di_giotto]);
    // L.marker([43.768040, 11.253175], {icon: Viola}).bindPopup("Ponte vecchio").addTo(map);
    // L.marker([43.7720606, 11.2550904], {icon: Ciano}).bindPopup("I am an orange leaf.").addTo(map);
    // L.marker([43.7730606, 11.2560904], {icon: Rosso}).bindPopup("I am an orange leaf.").addTo(map);
-
-
-
-
    var circle = L.circle([43.768040, 11.253175], {
             color: 'red',
             fillColor: '#f03',
@@ -41,7 +39,7 @@ var map = L.map('map').setView([43.65, 11.13], 8); // latitudine = 43.4586541 ; 
         }).addTo(map);
 L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
-    maxZoom: 20,
+    maxZoom: 18,
     id: 'mapbox/streets-v11',
     tileSize: 512,
     zoomOffset: -1,
@@ -60,7 +58,8 @@ var
     
     
     //= L.marker([43.768040, 11.253175]).bindPopup('Ponte vecchio').addTo(map),
-    ponte_vecchio= L.marker([43.768040, 11.253175], {icon: Viola}).bindPopup("Ponte vecchio").addTo(map), // 43.458654 ; 11.253158
+    ponte_vecchio= L.marker([43.768040, 11.253175], {icon: Bianco}).bindPopup("Ponte vecchio").addTo(map); // 43.458654 ; 11.253158
+    ponte_vecchio3= L.marker([43.768040, 11.253175], {icon: Blu}).bindPopup("Ponte vecchio").addTo(map);
     ponte_vecchio2= L.marker([43.768040, 11.253175]).bindPopup("Ponte vecchio").addTo(map), // 43.458654 ; 11.253158
     campanile_di_giotto = L.marker([43.7728676, 11.2556974]).bindPopup('Campanile di Giotto').addTo(map); // 43.7728676 ; 11.2556974
 
