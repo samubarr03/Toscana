e831f3c4a064a55a4a8101591fef06b835398dca
Notepadmerda <leonardo.cusa.2003@calvino.edu.it>
2022-05-22 12:02:22 +0200

    main

--- PROGETTO/percorsi.php
+++ PROGETTO/percorsi.php
@@ -163,7 +163,7 @@
 
             </div>
         </div>
-    </div></div></div></div>
+    </div></div></div>
  <br>
 
   <a href="#" class="back-to-top d-flex align-items-center justify-content-center"><i class="bi bi-arrow-up-short"></i></a>
